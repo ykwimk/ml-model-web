1d4475550f6b193403a6db1372030e5159d8ab70
ykwimk <ykwimk@gmail.com>
2024-12-13 12:02:43 +0900

    chore: eslint, prettier, tsconfig 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,88 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const oldConfig = {
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier-plugin-tailwindcss'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@next/next/no-img-element': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react/**',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'next',
+            group: 'builtin',
+            position: 'after',
+          },
+          {
+            pattern: 'next/**',
+            group: 'builtin',
+            position: 'after',
+          },
+          {
+            pattern: 'public/**',
+            group: 'unknown',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'next'],
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/display-name': 'off',
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+  },
+};
+
+const eslintConfig = [...compat.config(oldConfig)];
 
 export default eslintConfig;
